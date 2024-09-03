9b3872da2e48c6b067e0715a021ae12f4c37bb99
Your Name <victor.lopesvfk11@gmail.com>
2024-09-03 17:15:55 -0300

    feat: git add SingIn Screen Components

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-
-export function App() {
-  return (
-    <>
-      <h1>SingIn Project</h1>
-    </>
-  )
-}
---
+++ src/screens/singin.jsx
@@ -0,0 +1,7 @@
+export function SingIn(){
+    return (
+        <>
+            <h1>Olá Mundo!</h1>
+        </>
+    )
+}
